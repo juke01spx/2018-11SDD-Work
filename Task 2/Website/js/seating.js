55cc14f15b639315982cfc0324842b2ea93aeaa4
juke01spx <juke01spx@gmail.com>
2018-08-20 17:18:50 +1000

    Completed the visual side (for the most part) of the seat selection.

---
+++ Task 2/Website/js/seating.js
@@ -0,0 +1,363 @@
+//Start A Row
+
+function a1click() {
+	document.getElementById("a1").style.background = "red";
+}
+
+function a2click() {
+	document.getElementById("a2").style.background = "red";
+}
+
+function a3click() {
+	document.getElementById("a3").style.background = "red";
+}
+
+function a4click() {
+	document.getElementById("a4").style.background = "red";
+}
+
+function a5click() {
+	document.getElementById("a5").style.background = "red";
+}
+
+function a6click() {
+	document.getElementById("a6").style.background = "red";
+}
+
+function a7click() {
+	document.getElementById("a7").style.background = "red";
+}
+
+function a8click() {
+	document.getElementById("a8").style.background = "red";
+}
+
+function a9click() {
+	document.getElementById("a9").style.background = "red";
+}
+
+function a10click() {
+	document.getElementById("a10").style.background = "red";
+}
+
+function a11click() {
+	document.getElementById("a11").style.background = "red";
+}
+
+function a12click() {
+	document.getElementById("a12").style.background = "red";
+}
+
+//Start B Row
+
+function b1click() {
+	document.getElementById("b1").style.background = "red";
+}
+
+function b2click() {
+	document.getElementById("b2").style.background = "red";
+}
+
+function b3click() {
+	document.getElementById("b3").style.background = "red";
+}
+
+function b4click() {
+	document.getElementById("b4").style.background = "red";
+}
+
+function b5click() {
+	document.getElementById("b5").style.background = "red";
+}
+
+function b6click() {
+	document.getElementById("b6").style.background = "red";
+}
+
+function b7click() {
+	document.getElementById("b7").style.background = "red";
+}
+
+function b8click() {
+	document.getElementById("b8").style.background = "red";
+}
+
+function b9click() {
+	document.getElementById("b9").style.background = "red";
+}
+
+function b10click() {
+	document.getElementById("b10").style.background = "red";
+}
+
+function b11click() {
+	document.getElementById("b11").style.background = "red";
+}
+
+function b12click() {
+	document.getElementById("b12").style.background = "red";
+}
+
+//Start C Row
+
+function c1click() {
+	document.getElementById("c1").style.background = "red";
+}
+
+function c2click() {
+	document.getElementById("c2").style.background = "red";
+}
+
+function c3click() {
+	document.getElementById("c3").style.background = "red";
+}
+
+function c4click() {
+	document.getElementById("c4").style.background = "red";
+}
+
+function c5click() {
+	document.getElementById("c5").style.background = "red";
+}
+
+function c6click() {
+	document.getElementById("c6").style.background = "red";
+}
+
+function c7click() {
+	document.getElementById("c7").style.background = "red";
+}
+
+function c8click() {
+	document.getElementById("c8").style.background = "red";
+}
+
+function c9click() {
+	document.getElementById("c9").style.background = "red";
+}
+
+function c10click() {
+	document.getElementById("c10").style.background = "red";
+}
+
+function c11click() {
+	document.getElementById("c11").style.background = "red";
+}
+
+function c12click() {
+	document.getElementById("c12").style.background = "red";
+}
+
+function c13click() {
+	document.getElementById("c13").style.background = "red";
+}
+
+function c14click() {
+	document.getElementById("c14").style.background = "red";
+}
+
+function c15click() {
+	document.getElementById("c15").style.background = "red";
+}
+
+//Start D Row
+
+function d1click() {
+	document.getElementById("d1").style.background = "red";
+}
+
+function d2click() {
+	document.getElementById("d2").style.background = "red";
+}
+
+function d3click() {
+	document.getElementById("d3").style.background = "red";
+}
+
+function d4click() {
+	document.getElementById("d4").style.background = "red";
+}
+
+function d5click() {
+	document.getElementById("d5").style.background = "red";
+}
+
+function d6click() {
+	document.getElementById("d6").style.background = "red";
+}
+
+function d7click() {
+	document.getElementById("d7").style.background = "red";
+}
+
+function d8click() {
+	document.getElementById("d8").style.background = "red";
+}
+
+function d9click() {
+	document.getElementById("d9").style.background = "red";
+}
+
+function d10click() {
+	document.getElementById("d10").style.background = "red";
+}
+
+function d11click() {
+	document.getElementById("d11").style.background = "red";
+}
+
+function d12click() {
+	document.getElementById("d12").style.background = "red";
+}
+
+function d13click() {
+	document.getElementById("d13").style.background = "red";
+}
+
+function d14click() {
+	document.getElementById("d14").style.background = "red";
+}
+
+function d15click() {
+	document.getElementById("d15").style.background = "red";
+}
+
+//Start E Row
+
+function e1click() {
+	document.getElementById("e1").style.background = "red";
+}
+
+function e2click() {
+	document.getElementById("e2").style.background = "red";
+}
+
+function e3click() {
+	document.getElementById("e3").style.background = "red";
+}
+
+function e4click() {
+	document.getElementById("e4").style.background = "red";
+}
+
+function e5click() {
+	document.getElementById("e5").style.background = "red";
+}
+
+function e6click() {
+	document.getElementById("e6").style.background = "red";
+}
+
+function e7click() {
+	document.getElementById("e7").style.background = "red";
+}
+
+function e8click() {
+	document.getElementById("e8").style.background = "red";
+}
+
+function e9click() {
+	document.getElementById("e9").style.background = "red";
+}
+
+function e10click() {
+	document.getElementById("e10").style.background = "red";
+}
+
+function e11click() {
+	document.getElementById("e11").style.background = "red";
+}
+
+function e12click() {
+	document.getElementById("e12").style.background = "red";
+}
+
+function e13click() {
+	document.getElementById("e13").style.background = "red";
+}
+
+function e14click() {
+	document.getElementById("e14").style.background = "red";
+}
+
+function e15click() {
+	document.getElementById("e15").style.background = "red";
+}
+
+//Start F Row
+
+function f1click() {
+	document.getElementById("f1").style.background = "red";
+}
+
+function f2click() {
+	document.getElementById("f2").style.background = "red";
+}
+
+function f3click() {
+	document.getElementById("f3").style.background = "red";
+}
+
+function f4click() {
+	document.getElementById("f4").style.background = "red";
+}
+
+function f5click() {
+	document.getElementById("f5").style.background = "red";
+}
+
+function f6click() {
+	document.getElementById("f6").style.background = "red";
+}
+
+function f7click() {
+	document.getElementById("f7").style.background = "red";
+}
+
+function f8click() {
+	document.getElementById("f8").style.background = "red";
+}
+
+function f9click() {
+	document.getElementById("f9").style.background = "red";
+}
+
+function f10click() {
+	document.getElementById("f10").style.background = "red";
+}
+
+function f11click() {
+	document.getElementById("f11").style.background = "red";
+}
+
+function f12click() {
+	document.getElementById("f12").style.background = "red";
+}
+
+function f13click() {
+	document.getElementById("f13").style.background = "red";
+}
+
+function f14click() {
+	document.getElementById("f14").style.background = "red";
+}
+
+function f15click() {
+	document.getElementById("f15").style.background = "red";
+}
+
+function f16click() {
+	document.getElementById("f16").style.background = "red";
+}
+
+function f17click() {
+	document.getElementById("f17").style.background = "red";
+}
+
+function f18click() {
+	document.getElementById("f18").style.background = "red";
+}
+
+function f19click() {
+	document.getElementById("f19").style.background = "red";
+}
